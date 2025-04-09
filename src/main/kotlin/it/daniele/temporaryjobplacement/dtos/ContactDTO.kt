76cd62d8e4a7c22d9c0c9eb1d682f7d8d3bdaf90
DanieleF36 <daniele.femia@outlook.com>
2025-04-10 00:15:03 +0200

    added validation

--- src/main/kotlin/it/daniele/temporaryjobplacement/dtos/ContactDTO.kt
+++ src/main/kotlin/it/daniele/temporaryjobplacement/dtos/ContactDTO.kt
@@ -1,17 +1,19 @@
 package it.daniele.temporaryjobplacement.dtos
 
+import it.daniele.temporaryjobplacement.annotation.OptionalNotBlank
 import it.daniele.temporaryjobplacement.entities.contact.Category
 import it.daniele.temporaryjobplacement.entities.contact.Contact
+import jakarta.validation.constraints.NotBlank
 
 
 data class ContactDTO(
     val id: Int,
-    val name: String,
-    val surname: String,
-    val email: String?,
-    val address: String?,
-    val telephone: String?,
-    val ssn: String?,
+    @field:NotBlank val name: String,
+    @field:NotBlank val surname: String,
+    @field:OptionalNotBlank val email: String?,
+    @field:OptionalNotBlank val address: String?,
+    @field:OptionalNotBlank val telephone: String?,
+    @field:OptionalNotBlank val ssn: String?,
     val category: Category
 )
 
--- src/main/kotlin/it/daniele/temporaryjobplacement/dtos/MessageDTO.kt
+++ src/main/kotlin/it/daniele/temporaryjobplacement/dtos/MessageDTO.kt
@@ -1,20 +1,24 @@
 package it.daniele.temporaryjobplacement.dtos
 
 import it.daniele.temporaryjobplacement.entities.message.Channel
+import it.daniele.temporaryjobplacement.annotation.OptionalNotBlank
 import it.daniele.temporaryjobplacement.entities.message.Message
 import it.daniele.temporaryjobplacement.entities.message.State
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.Positive
 import java.time.ZonedDateTime
 
 data class MessageDTO(
-    val id: Int,
-    val sender: ContactDTO,
+    val id: Int = 0,
+    @Valid val sender: ContactDTO,
     val date: ZonedDateTime,
-    val subject: String?,
-    val body: String?,
     val channel: Channel,
-    val priority: Int,
     val state: State,
     val actions: List<ActionDTO>,
+    @field:OptionalNotBlank val subject: String?,
+    @field:OptionalNotBlank val body: String?,
+    @field:Min(0) val priority: Int,
 )
 
 fun Message.toDTO(): MessageDTO=
