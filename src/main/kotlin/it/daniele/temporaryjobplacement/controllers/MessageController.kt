44539a4a689d26bd50f9c4ad0480cf6089a5cb31
DanieleF36 <daniele.femia@outlook.com>
2025-04-10 00:22:56 +0200

    getAll added

--- src/main/kotlin/it/daniele/temporaryjobplacement/controllers/MessageController.kt
+++ src/main/kotlin/it/daniele/temporaryjobplacement/controllers/MessageController.kt
@@ -1,5 +1,30 @@
 package it.daniele.temporaryjobplacement.controllers
+import it.daniele.temporaryjobplacement.dtos.MessageDTO
+import it.daniele.temporaryjobplacement.entities.message.State
+import it.daniele.temporaryjobplacement.services.MessageService
+import jakarta.validation.constraints.Min
+import org.springframework.data.domain.Page
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 @RestController
 @RequestMapping("/API/messages")
 class MessageController(private val service: MessageService) {
+    @GetMapping
+    fun getAll(
+        @RequestParam(defaultValue = "0") @Min(0, message = "Page number must be >= 0") page: Int,
+        @RequestParam(defaultValue = "10") @Positive(message = "Limit number must be > 0")limit: Int,
+        @RequestParam sort: String?,
+        @RequestParam(defaultValue = "received") filter: String?
+    ): Page<MessageDTO> {
+        val state = filter?.let {
+            try {
+                State.valueOf(it.uppercase())
+            }catch (ex: IllegalArgumentException) {
+                throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Filter is not a valid $filter")
+            }
+        }
+
+        val allowedSort = listOf("sender", "date", "subject", "body", "channel", "priority", "state")
+        return service.getAll(page, limit, validateSort(allowedSort, sort, "date"), state)
+    }
 }
--- src/main/kotlin/it/daniele/temporaryjobplacement/services/MessageServiceImpl.kt
+++ src/main/kotlin/it/daniele/temporaryjobplacement/services/MessageServiceImpl.kt
@@ -1,6 +1,23 @@
 package it.daniele.temporaryjobplacement.services
+import it.daniele.temporaryjobplacement.dtos.MessageDTO
+import it.daniele.temporaryjobplacement.dtos.toDTO
+import it.daniele.temporaryjobplacement.entities.message.State
+import jakarta.transaction.Transactional
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 @Service
 @Transactional
 class MessageServiceImpl(
+    private val messageRepo: MessageRepository,
+    private val contactRepository: ContactRepository,
+    private val actionRepository: ActionRepository
 ): MessageService {
+    override fun getAll(page: Int, limit: Int, sort: Sort, state: State?): Page<MessageDTO> {
+        if (page < 0) throw IllegalArgumentException("Page must be >= 0")
+        if (limit <= 0) throw IllegalArgumentException("Limit must be > 0")
+        val pageable = PageRequest.of(page, limit, sort)
+        if(state != null)
+            return messageRepo.findByState(state, pageable).map { it.toDTO() }
+        return messageRepo.findAll(pageable).map { it.toDTO() }
+    }
 }
