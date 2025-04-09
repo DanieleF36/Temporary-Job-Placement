7aff41b67016562c80f4a03f17b134981cc4df99
DanieleF36 <daniele.femia@outlook.com>
2025-04-10 00:35:53 +0200

    removed channel and actions from DTO

--- src/main/kotlin/it/daniele/temporaryjobplacement/dtos/MessageDTO.kt
+++ src/main/kotlin/it/daniele/temporaryjobplacement/dtos/MessageDTO.kt
@@ -1,6 +1,5 @@
 package it.daniele.temporaryjobplacement.dtos
 
-import it.daniele.temporaryjobplacement.entities.message.Channel
 import it.daniele.temporaryjobplacement.annotation.OptionalNotBlank
 import it.daniele.temporaryjobplacement.entities.message.Message
 import it.daniele.temporaryjobplacement.entities.message.State
@@ -13,13 +12,11 @@ data class MessageDTO(
     val id: Int = 0,
     @Valid val sender: ContactDTO,
     val date: ZonedDateTime,
-    val channel: Channel,
     val state: State,
-    val actions: List<ActionDTO>,
     @field:OptionalNotBlank val subject: String?,
     @field:OptionalNotBlank val body: String?,
     @field:Min(0) val priority: Int,
 )
 
 fun Message.toDTO(): MessageDTO=
-    MessageDTO(this.getId(), sender.toDTO(), date, subject, body, channel, priority, state, actions.map { it.toDTO() })
+    MessageDTO(this.getId(), sender.toDTO(), date, state, subject, body, priority)
