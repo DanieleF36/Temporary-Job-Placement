5349c95d36d75389f4e9ed174d528610bfcf9ea4
DanieleF36 <daniele.femia@outlook.com>
2025-04-10 17:46:11 +0200

    added channel

--- src/main/kotlin/it/daniele/temporaryjobplacement/dtos/MessageDTO.kt
+++ src/main/kotlin/it/daniele/temporaryjobplacement/dtos/MessageDTO.kt
@@ -1,10 +1,12 @@
 package it.daniele.temporaryjobplacement.dtos
 
 import it.daniele.temporaryjobplacement.annotation.OptionalNotBlank
+import it.daniele.temporaryjobplacement.entities.message.Channel
 import it.daniele.temporaryjobplacement.entities.message.Message
 import it.daniele.temporaryjobplacement.entities.message.State
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 import java.time.ZonedDateTime
 
@@ -16,7 +18,8 @@ data class MessageDTO(
     @field:OptionalNotBlank val subject: String?,
     @field:OptionalNotBlank val body: String?,
     @field:Min(0) val priority: Int,
+    val channel: Channel
 )
 
 fun Message.toDTO(): MessageDTO=
-    MessageDTO(this.getId(), sender.toDTO(), date, state, subject, body, priority)
+    MessageDTO(this.getId(), sender.toDTO(), date, state, subject, body, priority, channel)
